064b5472a85416984eadc724af9158788327f933
Dipen <pateldipe1@ecs.vuw.ac.nz>
2019-01-30 23:11:48 +1300

    binding completed

--- src/app/log-info-page/log-info-page.page.ts
+++ src/app/log-info-page/log-info-page.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-log-info-page',
-  templateUrl: './log-info-page.page.html',
-  styleUrls: ['./log-info-page.page.scss'],
+    selector: 'app-log-info-page',
+    templateUrl: './log-info-page.page.html',
+    styleUrls: ['./log-info-page.page.scss'],
 })
 export class LogInfoPagePage implements OnInit {
+    title: any = 'Enter details here...';
+    fullName: string;
+    email: any;
+    constructor() {     }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+    subscribe(){    }
 
 }
